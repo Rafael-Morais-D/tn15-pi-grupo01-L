248d6c42130cfa9ac4286962916aebd52a5c60fa
thales-feitosa <contact.thales@gmail.com>
2020-07-18 15:12:40 -0300

    Criando tabela de mensagens no banco de dados

---
+++ app/Mensagem.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Mensagem extends Model
+{
+    protected $table = "produtos";
+    protected $fillable = ['nome', 'email', 'mensagem'];
+
+    public function setNomeAttribute($value){
+        $this->attributes['mensagem']
+    }
+}
