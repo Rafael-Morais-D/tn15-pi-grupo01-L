45feb1fbca0115e64a4125c322e2ebe5d485abcc
Shoyo <shoyo@hotmail.com>
2020-07-20 15:12:22 -0300

    pedidos/migration

---
+++ database/migrations/2020_07_20_163258_create_pedidos_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePedidosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pedidos', function (Blueprint $table) {
+            $table->id();
+            //$table->dateTime('created_at', 0);
+            //primary key from the "users" table
+            $table->unsignedBigInteger('id_usuario');
+            $table->foreign('id_usuario')->references('id')->on('usuarios');
+            //chave primária da tabela de produtos
+            $table->unsignedBigInteger('id_produto'); 
+            $table->foreign('id_produto')->references('id')->on('produtos');
+            $table->string('status', 15);
+            $table->decimal('total', 8,2);
+            $table->string('pagStatus', 20);
+            $table->timestamps();
+        });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pedidos');
+    }
+}
