e9ebcfce19a24cd9175695fd893a5e16fc3984a5
thales-feitosa <contact.thales@gmail.com>
2020-08-05 19:55:29 -0300

    Alterando a cor do link de paginação

--- resources/views/admin/adm-usuario.blade.php
+++ resources/views/admin/adm-usuario.blade.php
@@ -285,6 +285,9 @@
                     @endforeach
                 </tbody>
             </table>
+            <div class="d-flex justify-content-center mt-4">
+                {{ $users->links() }}
+            </div>
             
             @if(!empty(Request::get('success')))
             <div class="alert alert-success text-center col-md-12">
